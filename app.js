88de9792410509b3f6525c857fcc9e1f2eb6b671
anuj607 <anujkumar607@gmail.com>
2020-07-19 09:54:45 +0530

    Update app.js
    primary

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Azure Web App! new');
+  res.send('Hello World from Azure Web App! Primary');
 });
 app.get('/users', function (req, res) {
   res.send('Hello users');
