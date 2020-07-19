94cf3ddabea9a58c5fd32206f58315b3033d56f9
Anuj <anujkumar607@gmail.com>
2020-07-19 06:01:16 +0530

    first commit

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use(function(req, res, next) {
 app.get('/', function (req, res) {
   res.send('Hello World from Azure Web App!');
 });
+app.get('/users', function (req, res) {
+  res.send('Hello users');
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
