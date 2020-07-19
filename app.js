4a5925d3fea3e564cbba6538f7d7d61539c51f5d
Anuj <anujkumar607@gmail.com>
2020-07-19 06:03:58 +0530

    third commit

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Azure Web App!');
+  res.send('Hello World from Azure Web App! new');
 });
 app.get('/users', function (req, res) {
   res.send('Hello users');
